aa3ec93bc248c9998bee64d87986e64088f93f20
Ian Coleman <ian@iancoleman.io>
2019-08-13 12:54:40 +1000

    Add modal for settings

--- src/js/config.js
+++ src/js/config.js
@@ -11,4 +11,24 @@ config = {
         min: 1200,
         max: 1400,
     },
-}
+};
+
+(function() {
+
+    let DOM = {};
+    DOM.settingsShow = document.getElementById("settings-show");
+    DOM.settingsHide = document.getElementById("settings-hide");
+    DOM.settingsModal = document.getElementById("settings-modal");
+
+    DOM.settingsShow.addEventListener("click", showSettings);
+    DOM.settingsHide.addEventListener("click", hideSettings);
+
+    function showSettings() {
+        DOM.settingsModal.classList.remove("hidden");
+    }
+
+    function hideSettings() {
+        DOM.settingsModal.classList.add("hidden");
+    }
+
+})();
