71e43d3cd5cd460c78c54ccd07a3abf83c37a467
Ian Coleman <ian@iancoleman.io>
2019-08-15 09:58:05 +1000

    Change node.bid to allow custom calcs of the value
    A user would submit a string to node.setCalcBidFn(s) containing the
    logic for updating the bid.
    The logic takes no inputs and returns a bid value, eg a valid input to
    setCalcBidFn would be
    "function() { return 1205; }"

--- src/js/node.js
+++ src/js/node.js
@@ -7,10 +7,38 @@ Node = function() {
     container.innerHTML = template;
     this.el = container.querySelectorAll("*")[0];
 
-    this.bid = prng.nextRange(config.bid.min, config.bid.max);
+    this.bid = null;
 
     this.totalRewards = 0;
 
-    this.el.textContent = this.bid;
+    // This function can be set by the user.
+    // It simply calculates and returns a bid value.
+    // It doesn't apply that bid to this node.
+    // Indentation is unusual for this so that when calcBid.toString()
+    // is called the indentation will be for a non-nested function.
+    this.calcBid = function() {
+    // if no bid, generate a random bid
+    if (!self.bid) {
+        return prng.nextRange(config.bid.min, config.bid.max);
+    }
+    // return the current bid
+    return self.bid
+}
+
+    this.setCalcBidFn = function(fnStr) {
+        eval("self.calcBid = " + fnStr);
+    }
+
+    this.updateBid = function() {
+        self.bid = self.calcBid();
+        render();
+    }
+
+    function render() {
+        self.el.textContent = self.bid;
+    }
+
+    self.updateBid();
+    render();
 
 }
