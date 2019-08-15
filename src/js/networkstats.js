40b3d8e47335da5272e843506b32b349b34e78a6
Ian Coleman <ian@iancoleman.io>
2019-08-16 09:19:25 +1000

    Add overall network stats

---
+++ src/js/networkstats.js
@@ -0,0 +1,23 @@
+(function() {
+
+    let mmEl = document.querySelectorAll(".median-medians")[0];
+    let aaEl = document.querySelectorAll(".average-averages")[0];
+    let amEl = document.querySelectorAll(".average-medians")[0];
+    let maEl = document.querySelectorAll(".median-averages")[0];
+
+    function renderNetworkStats() {
+        let medians = network.sections.map(function(s) { return s.median() });
+        let averages = network.sections.map(function(s) { return s.average() });
+        let mm = Stats.median(medians);
+        let aa = Stats.average(averages);
+        let am = Stats.average(medians);
+        let ma = Stats.median(averages);
+        mmEl.textContent = mm.toFixed(1);
+        aaEl.textContent = aa.toFixed(1);
+        amEl.textContent = am.toFixed(1);
+        maEl.textContent = ma.toFixed(1);
+    }
+
+    eventsource.afterNbEvent(renderNetworkStats);
+
+})();
